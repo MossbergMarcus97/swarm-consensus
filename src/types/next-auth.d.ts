c1180fd3565d23a8fe4105c9d071b38799a8e72d
Marcus Mossberg <marcus@kvikk.no>
2025-11-16 14:39:16 +0100

    Fix session user typing

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,21 @@
+import type { DefaultSession } from "next-auth";
+
 declare module "next-auth" {
   interface Session {
-    expires: string;
-    user?: {
-      id?: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
   }
 }
 
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
 
+export {};
