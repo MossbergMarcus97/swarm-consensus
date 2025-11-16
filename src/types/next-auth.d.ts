b5c59b1502334e4fccb85321c435ebae5bcccf7d
Marcus Mossberg <marcus@kvikk.no>
2025-11-16 14:44:34 +0100

    Fix session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,19 +2,19 @@ import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: string;
+    user?: {
+      id?: string;
     } & DefaultSession["user"];
   }
 
   interface User {
-    id: string;
+    id?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string;
+    id?: string;
   }
 }
 
